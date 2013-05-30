57ea9f6e80dfba3bb6a4cffe72a46a261502b1d0
Brooke Bryan <brooke@justdevelop.it>
2013-05-30 09:55:21 +0100

    additional namespace checks

--- src/Benchmark/Cli/GetNamespace.php
+++ src/Benchmark/Cli/GetNamespace.php
@@ -10,32 +10,44 @@ use Cubex\Core\Http\Request;
 class GetNamespace extends BenchmarkCommand
 {
   public $reflect = false;
-  public $strchr = false;
+  public $stringRun = false;
 
   public function runTest()
   {
-    $source = '\Request';
-
-    if(!$this->reflect)
+    if($this->stringRun)
     {
-      if($this->strchr)
-      {
-        $result = substr($source, 0, strchr($source, '\\'));
-      }
-      else
+      $source = 'Cubex\Core\Http\Request';
+
+      if(!$this->reflect)
       {
         $source = explode('\\', $source);
         array_pop($source);
         $result = implode('\\', $source);
       }
+      else
+      {
+        $refelction = new \ReflectionClass($source);
+        $result     = $refelction->getNamespaceName();
+      }
     }
     else
     {
-      $sourceObjectRefelction = new \ReflectionClass($source);
-      $result                 = $sourceObjectRefelction->getNamespaceName();
+      $source = new Request();
+
+      if(!$this->reflect)
+      {
+        $source = is_object($source) ? get_class($source) : $source;
+        $source = explode('\\', $source);
+        array_pop($source);
+        $result = implode('\\', $source);
+      }
+      else
+      {
+        $refelction = new \ReflectionObject($source);
+        $result     = $refelction->getNamespaceName();
+      }
     }
 
-    var_dump($result);
     return $result;
   }
 }
